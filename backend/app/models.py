40b81b3415804b39d25a6b77981c9cd1df1133ac
Pablo S S da Costa <pablo.ufopa@gmail.com>
2026-02-07 22:04:11 -0300

    feat: create model User

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+	class Role(models.TextChoices):
+	    ADMIN = 'admin', 'Administrador'
+	    CUSTOMER = 'user', 'Usuário'
+
+	email = models.CharField(max_length=60, unique=True)
+	full_name = models.CharField(max_length=60)
+	cpf = models.CharField(max_length=11)
+	role = models.CharField(max_length=20, choices=Role.choices)
