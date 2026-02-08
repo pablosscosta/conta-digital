2dd1b88d5872f1b68ba9d12268d48465e485f884
Pablo S S da Costa <pablo.ufopa@gmail.com>
2026-02-08 09:18:40 -0300

    refactor: adjust User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	class Role(models.TextChoices):
 	    ADMIN = 'admin', 'Administrador'
-	    CUSTOMER = 'user', 'Usuário'
+	    USER = 'user', 'Usuário'
 
-	email = models.CharField(max_length=60, unique=True)
+	email = models.EmailField(max_length=254, unique=True)
 	full_name = models.CharField(max_length=60)
 	cpf = models.CharField(max_length=11)
 	role = models.CharField(max_length=20, choices=Role.choices)
